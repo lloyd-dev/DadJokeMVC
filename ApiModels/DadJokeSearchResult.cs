494d668bd3d915985a5e6a4eb76e5d2ca5e3f1ff
lloyd-dev <82885660+lloyd-dev@users.noreply.github.com>
2021-04-21 11:45:06 -0400

    An outer paging object. The schema for Dad Jokes objects returned in the search is different for those returned in a random joke fetch

---
+++ ApiModels/DadJokeSearchResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DadJokeMVC.ApiModels
+{
+    public class DadJokeSearchResult
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("joke")]
+        public string Joke { get; set; }
+    }
+}
---
+++ ApiModels/PagedDadJokeResponse.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DadJokeMVC.ApiModels
+{
+    public class PagedDadJokeResponse
+    {
+        [JsonProperty("current_page")]
+        public long CurrentPage { get; set; }
+
+        [JsonProperty("limit")]
+        public long Limit { get; set; }
+
+        [JsonProperty("next_page")]
+        public long NextPage { get; set; }
+
+        [JsonProperty("previous_page")]
+        public long PreviousPage { get; set; }
+
+        [JsonProperty("results")]
+        public DadJokeSearchResult[] Results { get; set; }
+
+        [JsonProperty("search_term")]
+        public string SearchTerm { get; set; }
+
+        [JsonProperty("status")]
+        public long Status { get; set; }
+
+        [JsonProperty("total_jokes")]
+        public long TotalJokes { get; set; }
+
+        [JsonProperty("total_pages")]
+        public long TotalPages { get; set; }
+    }
+}
+
