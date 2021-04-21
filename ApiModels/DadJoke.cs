436bb7564e25c0128b3c3fba564d8fd38913534d
lloyd-dev <82885660+lloyd-dev@users.noreply.github.com>
2021-04-21 08:33:25 -0400

    API response model for DadJoke

---
+++ ApiModels/DadJoke.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DadJokeMVC.ApiModels
+{
+    public class DadJoke
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("joke")]
+        public string Joke { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+    }
+}
