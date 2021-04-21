e42aefc5dafb1675c68e8e37394194e4447309dd
lloyd-dev <82885660+lloyd-dev@users.noreply.github.com>
2021-04-21 13:08:11 -0400

    DadJoke Controller. Random and Search return json. Plan is to have a home view that will call them from javascript

--- Controllers/DadJokeController.cs
+++ Controllers/DadJokeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DadJokeMVC.Services;
+using DadJokeMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +11,31 @@ namespace DadJokeMVC.Controllers
 {
     public class DadJokeController : Controller
     {
+        private readonly IDadJokeAPIService dadJokeAPIService;
+        private readonly ILogger<DadJokeController> logger;
+        
+        public DadJokeController(IDadJokeAPIService dadJokeAPIService, ILogger<DadJokeController> logger) {
+            this.dadJokeAPIService = dadJokeAPIService;
+            this.logger = logger;
+        }
 
 
+        public IActionResult DadJokeHome() {
+            // create a div in the view to hold the random joke that gets returned. and a button to trigger the call 
+            var viewmodel = new DadJokeHomeViewModel();
+
+            return View("DadJokeHome", viewmodel);
+        }
+        public JsonResult Random() {
+
+            var randomJoke = this.dadJokeAPIService.GetRandom();           
+            return Json(randomJoke.Result.Joke);
+        }
+
+        public JsonResult Search(string searchText) {
+
+            var results = this.dadJokeAPIService.SearchByText(searchText);
+            return Json(results); 
+        }
     }
 }
