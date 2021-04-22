738109a4112c9545bf0cd8c2a909b4a9da3f6c4b
lloyd-dev <82885660+lloyd-dev@users.noreply.github.com>
2021-04-22 17:45:03 -0400

    Don't need the view model stuff since it's getting the data from jsonresult methods.

--- Controllers/DadJokeController.cs
+++ Controllers/DadJokeController.cs
@@ -1,5 +1,4 @@
 ﻿using DadJokeMVC.Services;
-using DadJokeMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DadJokeMVC.Controllers
@@ -10,24 +9,14 @@ namespace DadJokeMVC.Controllers
         
         public DadJokeController(IDadJokeAPIService dadJokeAPIService) {
             this.dadJokeAPIService = dadJokeAPIService;
-        }
-
-
-        public IActionResult DadJokeHome() {
-            // create a div in the view to hold the random joke that gets returned. and a button to trigger the call 
-            var viewmodel = new DadJokeHomeViewModel();
-
-            return View("DadJokeHome", viewmodel);
-        }
-
-        // in case I don't get to finish the view stuff, navigate to this endpoint @ /dadjoke/random
+        }        
+       
         public JsonResult Random() {
 
             var randomJoke = this.dadJokeAPIService.GetRandom();           
             return Json(randomJoke.Result.Joke);
         }
-
-        // in case I don't get to finish the view stuff, navigate to this endpoint @ /dadjoke/search?searchText=
+        
         public JsonResult Search(string searchText, int limit = 30, int page = 1) {
 
             var results = this.dadJokeAPIService.SearchByText(searchText, limit, page);    
